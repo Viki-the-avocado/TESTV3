35d32f246a8618cccdcef3566a7c630384529ab6
Viksa <45698798+Viki-the-avocado@users.noreply.github.com>
2022-11-22 10:29:43 +0100

    Add files via upload

---
+++ lib/helpers/Constant.dart
@@ -0,0 +1,56 @@
+const appName = 'Prime Web';
+
+const String appbartitle = 'Prime Web';
+const String andoidPackageName = 'com.wrteam.prime_web';
+
+//change this url to set your URL in app
+const String webinitialUrl = 'https://wrteam.in/';
+const String firstTabUrl = 'https://www.google.com';
+
+//keep local content of pages of setting screen
+const String aboutPageURL = 'https://wrteam.in/about-us/';
+const String privacyPageURL = '';
+const String termsPageURL = '';
+
+//Change App id of android and IOS app
+const String androidAppId = andoidPackageName;
+
+const String iOSAppId = '';
+
+const String shareAppTitle = appName;
+const String shareiOSAppMessage = '';
+const String shareAndroidAppMessge =
+    'Download $appName App from this link : http://play.google.com/store/apps/details?id=$androidAppId';
+
+//To turn on/off ads
+const bool showInterstitialAds = true;
+const bool showBannerAds = true;
+const bool showOpenAds = true;
+
+//To turn on/off display of bottom navigation bar
+const bool showBottomNavigationBar = false;
+
+//To show/remove splash screen
+const bool showSplashScreen = true;
+
+//To remove/display header/footer of website
+const bool hideHeader = false;
+const bool hideFooter = false;
+
+//Ids for interstitial Ad
+const androidInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+const iosInterstitialAdId = 'ca-app-pub-3940256099942544/4411468910';
+
+//Ids for banner Ad
+const androidBannerAdId = 'ca-app-pub-3940256099942544/6300978111';
+const iosBannerAdId = 'ca-app-pub-3940256099942544/2934735716';
+
+//Ids for app open Ad
+const androidOpenAdId = 'ca-app-pub-3940256099942544/3419835294';
+const iosOpenAdId = 'ca-app-pub-3940256099942544/5662855259';
+
+//icon to set when get firebase messages
+const String notificationIcon = '@mipmap/ic_launcher_squircle';
+
+//path to icons   *** don't change these values**
+const String iconPath = 'assets/icons/';
